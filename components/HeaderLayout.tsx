4413e26b91b3ff862dee29b6fa260e327ff8d4f9
nam <npnam031101@gmail.com>
2024-10-27 08:27:50 -0500

    update

--- components/HeaderLayout.tsx
+++ components/HeaderLayout.tsx
@@ -19,7 +19,7 @@ export default function HeaderLayout() {
     };
 
     return (
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 border-b sticky top-0 bg-white z-50">
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 border-b w-full sticky top-0 bg-white z-50">
             <div className="flex items-center gap-2 px-4">
                 <SidebarTrigger className="-ml-1 block md:hidden" />
                 <Separator orientation="vertical" className="mr-2 h-4 block md:hidden" />
