d748ea9fd06ccdee823fe18689e5047b182f541f
Nguyễn Phương Nam <119719970+npnamdev@users.noreply.github.com>
2024-11-09 23:22:27 +0700

    Update DrawerDemo.tsx

--- components/DrawerDemo.tsx
+++ components/DrawerDemo.tsx
@@ -67,7 +67,7 @@ export function DrawerDemo() {
   return (
     <Drawer>
       <DrawerTrigger asChild>
-        <Button variant="outline">Open Drawer</Button>
+        <Button variant="outline">Drawer</Button>
       </DrawerTrigger>
       <DrawerContent>
         <div className="mx-auto w-full max-w-sm">
