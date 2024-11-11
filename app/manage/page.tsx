1ef5210efe48954244b287e02348306ecb0dab96
Nguyễn Phương Nam <119719970+npnamdev@users.noreply.github.com>
2024-11-11 22:39:33 +0700

    Update page.tsx

--- app/manage/page.tsx
+++ app/manage/page.tsx
@@ -5,10 +5,10 @@ export default function DashBoardPage() {
   return (
     <div className="flex flex-1 flex-col gap-4 py-2 md:py-4 px-4">
       <div className="grid auto-rows-min gap-3 md:gap-4 grid-cols-2 md:grid-cols-4">
-        <Skeleton className="aspect-video rounded-xl bg-wwhite shadow-md bg-white" />
-        <Skeleton className="aspect-video rounded-xl bg-wwhite shadow-md bg-white" />
-        <Skeleton className="aspect-video rounded-xl bg-wwhite shadow-md bg-white" />
-        <Skeleton className="aspect-video rounded-xl bg-wwhite shadow-md bg-white" />
+        <Skeleton className="aspect-video rounded-xl bg-white shadow border bg-white" />
+        <Skeleton className="aspect-video rounded-xl bg-white shadow border bg-white" />
+        <Skeleton className="aspect-video rounded-xl bg-white shadow border bg-white" />
+        <Skeleton className="aspect-video rounded-xl bg-white shadow border bg-white" />
       </div>
       <BarChatDemo />
     </div>
