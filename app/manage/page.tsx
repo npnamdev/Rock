92afe4f2761849f908a7cbcec5bf8ba55238c5a5
Nguyễn Phương Nam <119719970+npnamdev@users.noreply.github.com>
2024-11-11 21:46:38 +0700

    Update page.tsx

--- app/manage/page.tsx
+++ app/manage/page.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default function DashBoardPage() {
   return (
-    <div className="flex flex-1 flex-col gap-4 p-4">
+    <div className="flex flex-1 flex-col gap-3 md:gap-4 p-4">
       <div className="grid auto-rows-min gap-4 grid-cols-2 md:grid-cols-4">
         <Skeleton className="aspect-video rounded-xl bg-wwhite shadow-md bg-white" />
         <Skeleton className="aspect-video rounded-xl bg-wwhite shadow-md bg-white" />
