696ea78775c39b3641842fc4babe8ce684b6fdd9
Nam dev <npnam031101@gmail.com>
2024-11-12 18:24:08 +0700

    update

--- app/manage/page.tsx
+++ app/manage/page.tsx
@@ -1,62 +1,15 @@
 import { BarChatDemo } from "@/components/BarChatDemo";
 import { Skeleton } from "@/components/ui/skeleton";
-import * as React from "react"
-import Image from "next/image"
-
-import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area"
-
-export interface Artwork {
-  artist: string
-  art: string
-}
-
-export const works: Artwork[] = [
-  {
-    artist: "Ornella Binni",
-    art: "https://images.unsplash.com/photo-1465869185982-5a1a7522cbcb?auto=format&fit=crop&w=300&q=80",
-  },
-  {
-    artist: "Tom Byrom",
-    art: "https://images.unsplash.com/photo-1548516173-3cabfa4607e9?auto=format&fit=crop&w=300&q=80",
-  },
-  {
-    artist: "Vladimir Malyavko",
-    art: "https://images.unsplash.com/photo-1494337480532-3725c85fd2ab?auto=format&fit=crop&w=300&q=80",
-  },
-  {
-    artist: "Ornella Binni",
-    art: "https://images.unsplash.com/photo-1465869185982-5a1a7522cbcb?auto=format&fit=crop&w=300&q=80",
-  },
-]
 
 export default function DashBoardPage() {
   return (
     <div className="flex flex-1 flex-col gap-4 py-2 md:py-4 px-4">
-      <ScrollArea className="w-full whitespace-nowrap rounded-md border bg-white">
-        <div className="flex w-max space-x-4 p-4">
-          {works.map((artwork) => (
-            <figure key={artwork.artist} className="shrink-0">
-              <div className="overflow-hidden rounded-md">
-                <Image
-                  src={artwork.art}
-                  alt={`Photo by ${artwork.artist}`}
-                  className="aspect-[16/9] h-fit w-fit object-cover"
-                  width={300}
-                  height={400}
-                />
-              </div>
-            </figure>
-          ))}
-        </div>
-        <ScrollBar orientation="horizontal" />
-      </ScrollArea>
-
-      {/* <div className="grid auto-rows-min gap-3 md:gap-4 grid-cols-2 md:grid-cols-4">
+      <div className="grid auto-rows-min gap-3 md:gap-4 grid-cols-2 md:grid-cols-4">
         <Skeleton className="aspect-video rounded-xl bg-white shadow border" />
         <Skeleton className="aspect-video rounded-xl bg-white shadow border" />
         <Skeleton className="aspect-video rounded-xl bg-white shadow border" />
         <Skeleton className="aspect-video rounded-xl bg-white shadow border" />
-      </div> */}
+      </div>
       <BarChatDemo />
     </div>
   );
