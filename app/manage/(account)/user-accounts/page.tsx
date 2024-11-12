6fb4b5abc5df59a2aca96d753d9abb1b51501959
Nguyễn Phương Nam <119719970+npnamdev@users.noreply.github.com>
2024-11-13 00:52:43 +0700

    Update page.tsx

--- app/manage/(account)/user-accounts/page.tsx
+++ app/manage/(account)/user-accounts/page.tsx
@@ -248,7 +248,7 @@ export default function UserAccountsPage() {
                                         </div>
                                     </TableCell>
                                     <TableCell className="h-[50px] px-4 cursor-pointer whitespace-nowrap">{user?.role?.name}</TableCell>
-                                    <TableCell className="h-[50px] px-4 cursor-pointer whitespace-nowrap">{moment(user.createdAt).format('lll')}</TableCell>
+                                    <TableCell className="h-[50px] px-4 cursor-pointer whitespace-nowrap">{moment(role.createdAt).subtract(10, 'days').calendar()}</TableCell>
                                     <TableCell className="h-[50px] px-4 cursor-pointer whitespace-nowrap">
                                         <UserActionMenu options={menuOptions} userID={user?._id} />
                                     </TableCell>
