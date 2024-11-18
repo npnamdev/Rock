b93d7442147adce92bd687aee762bb1d9f5387b9
Nam dev <npnam031101@gmail.com>
2024-11-18 10:24:46 +0700

    update

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,9 @@
+import { SettingsDialog } from "@/components/SettingsDialog";
+
+export function Page() {
+  return (
+    <div className="flex h-svh items-center justify-center">
+      <SettingsDialog />
+    </div>
+  )
+}
--- components/TeamSwitcher.tsx
+++ components/TeamSwitcher.tsx
@@ -3,15 +3,7 @@
 import * as React from "react"
 import { ChevronsUpDown, Plus } from "lucide-react"
 
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuShortcut,
-    DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+import { DropdownMenu, DropdownMenuContent,  DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuShortcut, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 import {
     SidebarMenu,
     SidebarMenuButton,
