8eeada36e22ab775593dc46627e854fd305308cc
Nguyễn Phương Nam <119719970+npnamdev@users.noreply.github.com>
2024-11-18 08:52:35 -0800

    Update AdminRoute.tsx

--- components/AdminRoute.tsx
+++ components/AdminRoute.tsx
@@ -5,6 +5,7 @@ import { useRouter } from 'next/navigation';
 
 const AdminRoute: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     const [isAuthorized, setIsAuthorized] = useState<boolean>(false);
+    const [loading, setLoading] = useState<boolean>(true);
     const router = useRouter();
 
     useEffect(() => {
@@ -12,7 +13,12 @@ const AdminRoute: React.FC<{ children: React.ReactNode }> = ({ children }) => {
             try {
                 const token = localStorage.getItem("accessToken");
                 if (!token) { router.push('/login'); return; }
-                setIsAuthorized(true);
+                
+                // Giả lập thời gian loading 1 giây
+                setTimeout(() => {
+                    setIsAuthorized(true);
+                    setLoading(false);
+                }, 1000);
             } catch (error) {
                 console.error("Error checking admin access:", error);
                 window.location.replace("/login");
@@ -22,8 +28,8 @@ const AdminRoute: React.FC<{ children: React.ReactNode }> = ({ children }) => {
         checkAdminAccess();
     }, []);
 
-    if (isAuthorized === false) {
-        return <div>Loading...</div>;
+    if (loading) {
+        return <div className="w-full h-dvh bg-blue-500 flex justify-center items-center text-white">Loading...</div>;
     }
 
     return <>{children}</>;
